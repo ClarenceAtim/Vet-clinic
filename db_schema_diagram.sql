210e9f957dfdbca4405c764a98f9329f4f875f62
RileyManda <rileymanda69@gmail.com>
2023-08-28 17:03:25 +0200

    Treatments to invoice indexes

--- db_schema_diagram.sql
+++ db_schema_diagram.sql
@@ -26,10 +26,11 @@ CREATE TABLE invoice_items (
 ALTER TABLE invoice_items
     ADD CONSTRAINT fk_invoices
     FOREIGN KEY (invoice_id) REFERENCES invoices(id);
-    
+
 ALTER TABLE invoice_items
     ADD CONSTRAINT fk_treatments
     FOREIGN KEY (treatment_id) REFERENCES treatments(id);
 
 -- invoice index keys 
 CREATE INDEX idx_invoice_items_invoice_id ON invoice_items(invoice_id);
+CREATE INDEX idx_invoice_items_treatment_id ON invoice_items(treatment_id);
