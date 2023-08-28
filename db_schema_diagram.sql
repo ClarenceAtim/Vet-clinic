4221be52e88eab6fb1e652e8f2907af24875440d
clarence <tegumclarence@gmail.com>
2023-08-28 19:47:18 +0100

    Created the table for patient, medical history

--- db_schema_diagram.sql
+++ db_schema_diagram.sql
@@ -1,5 +1,16 @@
 CREATE DATABASE clinic;
+CREATE TABLE patients (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  date_of_birth DATE NOT NULL
+);
 
+CREATE TABLE medical_histories (
+  id SERIAL PRIMARY KEY,
+  admitted_at TIMESTAMP,
+  patient_id INT NOT NULL,
+  status VARCHAR(150)
+);
 CREATE TABLE treatments (
     id SERIAL PRIMARY KEY,
     type VARCHAR(255),
@@ -32,5 +43,6 @@ ALTER TABLE invoice_items
     FOREIGN KEY (treatment_id) REFERENCES treatments(id);
 
 -- invoice index keys 
+
 CREATE INDEX idx_invoice_items_invoice_id ON invoice_items(invoice_id);
 CREATE INDEX idx_invoice_items_treatment_id ON invoice_items(treatment_id);
