d0b63f434d0463843a24f565fa55805c49684115
RileyManda <rileymanda69@gmail.com>
2023-08-28 16:37:20 +0200

    Included animals schema image to project|created invoice and invoice-items tables and indexes

--- db_schema_diagram.sql
+++ db_schema_diagram.sql
@@ -1,1 +1,18 @@
 CREATE DATABASE clinic;
+
+CREATE TABLE invoices (
+    id SERIAL PRIMARY KEY,
+    total_amount DECIMAL,
+    generated_at TIMESTAMP,
+    paid_at TIMESTAMP,
+    medical_history_id INTEGER
+);
+
+CREATE TABLE invoice_items (
+    id SERIAL PRIMARY KEY,
+    unit_price DECIMAL,
+    quantity INTEGER,
+    total_price DECIMAL,
+    invoice_id INTEGER,
+    treatment_id INTEGER
+);
