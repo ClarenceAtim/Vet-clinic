c86f44d10e5682d666573a6b076ebf71a385c3be
RileyManda <rileymanda69@gmail.com>
2023-08-29 13:09:43 +0200

    Implemented index on invoices

--- db_schema_diagram.sql
+++ db_schema_diagram.sql
@@ -68,3 +68,5 @@ CREATE INDEX idx_medical_histories_treatments_treatment_id ON medical_histories_
 CREATE INDEX idx_invoice_items_invoice_id ON invoice_items(invoice_id);
 CREATE INDEX idx_invoice_items_treatment_id ON invoice_items(treatment_id);
 CREATE INDEX idx_medical_histories_patient_id ON medical_histories(patient_id);
+CREATE INDEX ON invoices (medical_history_id);
+
